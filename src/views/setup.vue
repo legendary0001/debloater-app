bf04c5750d6be8bfa63d5e4190c0e5b422940d72
legendary0001 <jagritjtn@gmail.com>
2023-05-10 16:22:40 +0530

    hmmm

--- src/views/setup.vue
+++ src/views/setup.vue
@@ -71,10 +71,12 @@
         <div class="card1">
           <h3>USB Debugging Instructions:</h3>
           <ul>
-            <li style="text-align: center; font-weight: bold;"  >Requirements: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" style="fill:#3DDC84; vertical-align: bottom;" viewBox="0 0 918.6 515.1" enable-background="new 0 0 918.6 515.1"><g id="XMLID_1_"><path class="st0" d="M918.6 515.1h-918.6c14.7-155.7 103.7-288.7 235.1-359.9l-76.2-132c-4.3-7.4-1.8-16.8 5.6-21.1s16.8-1.8 21.1 5.6l77.2 133.7c58.9-26.9 125.2-41.9 196.5-41.9s137.6 15 196.5 41.9l77.2-133.7c4.2-7.4 13.7-9.9 21-5.6s9.9 13.7 5.6 21.1l-76.2 132c131.5 71.2 220.5 204.2 235.2 359.9zm-248.5-129c21.3 0 38.6-17.3 38.5-38.5 0-21.2-17.2-38.5-38.5-38.5-21.2 0-38.5 17.2-38.5 38.5 0 21.2 17.2 38.5 38.5 38.5zm-421.7 0c21.3 0 38.6-17.3 38.5-38.5 0-21.2-17.2-38.5-38.5-38.5-21.2 0-38.5 17.2-38.5 38.5 0 21.2 17.2 38.5 38.5 38.5z"/></g></svg> ANDROID 4.4 </li>
+            <li style="text-align: center; font-weight: bold;"  >Requirements: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" style="fill:#3DDC84; vertical-align: bottom;" viewBox="0 0 918.6 515.1" enable-background="new 0 0 918.6 515.1"><g id="XMLID_1_"><path class="st0" d="M918.6 515.1h-918.6c14.7-155.7 103.7-288.7 235.1-359.9l-76.2-132c-4.3-7.4-1.8-16.8 5.6-21.1s16.8-1.8 21.1 5.6l77.2 133.7c58.9-26.9 125.2-41.9 196.5-41.9s137.6 15 196.5 41.9l77.2-133.7c4.2-7.4 13.7-9.9 21-5.6s9.9 13.7 5.6 21.1l-76.2 132c131.5 71.2 220.5 204.2 235.2 359.9zm-248.5-129c21.3 0 38.6-17.3 38.5-38.5 0-21.2-17.2-38.5-38.5-38.5-21.2 0-38.5 17.2-38.5 38.5 0 21.2 17.2 38.5 38.5 38.5zm-421.7 0c21.3 0 38.6-17.3 38.5-38.5 0-21.2-17.2-38.5-38.5-38.5-21.2 0-38.5 17.2-38.5 38.5 0 21.2 17.2 38.5 38.5 38.5z"/></g></svg> ANDROID 4.4+ </li>
             <li style="text-align: center; font-weight: bold;">Note: this method can only debloat other phones not your phone<br>TO debloat your phone use wireless debugging or our windows app</li>
+           <li>Please follow first step(First enable developer options) before cotinuing</li>
             <li>1. Connect your phone to the other phone using usb (A/B/C) TO usb (A/B/C) cable</li>
             <li @click="opendevsetings">2. <a>On your device, go to Settings > Developer options > USB debugging.<br>(click this text to go to this setting page)</a></li>
+            <li>NOTE: to return from settings screen back to the app ,press back button</li>
             <li>3. Turn on usb debugging.</li>
           </ul>
         </div>
@@ -82,17 +84,19 @@
         <div class="card2">
           <h3>Wireless Debugging Instructions:</h3>
           <ul>
-            <li>Connect your device to your computer via USB cable.</li>
-            <li>On your device, go to Settings > Developer options > Wireless debugging and toggle the switch to turn it
-              on.</li>
-            <li>On your computer, open a terminal or command prompt and enter the following command:
-              <code>adb tcpip 5555</code></li>
-            <li>Disconnect your device from the USB cable.</li>
-            <li>Find the IP address of your device by going to Settings > About phone > Status > IP address.</li>
-            <li>On your computer, enter the following command in the terminal or command prompt:
-              <code>adb connect [device IP address]:5555</code></li>
-            <li>If successful, you should see a message saying "connected to [device IP address]:5555".</li>
-            <li>You can now use adb commands wirelessly.</li>
+            <li style="text-align: center; font-weight: bold;"  >Requirements: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" style="fill:#3DDC84; vertical-align: bottom;" viewBox="0 0 918.6 515.1" enable-background="new 0 0 918.6 515.1"><g id="XMLID_1_"><path class="st0" d="M918.6 515.1h-918.6c14.7-155.7 103.7-288.7 235.1-359.9l-76.2-132c-4.3-7.4-1.8-16.8 5.6-21.1s16.8-1.8 21.1 5.6l77.2 133.7c58.9-26.9 125.2-41.9 196.5-41.9s137.6 15 196.5 41.9l77.2-133.7c4.2-7.4 13.7-9.9 21-5.6s9.9 13.7 5.6 21.1l-76.2 132c131.5 71.2 220.5 204.2 235.2 359.9zm-248.5-129c21.3 0 38.6-17.3 38.5-38.5 0-21.2-17.2-38.5-38.5-38.5-21.2 0-38.5 17.2-38.5 38.5 0 21.2 17.2 38.5 38.5 38.5zm-421.7 0c21.3 0 38.6-17.3 38.5-38.5 0-21.2-17.2-38.5-38.5-38.5-21.2 0-38.5 17.2-38.5 38.5 0 21.2 17.2 38.5 38.5 38.5z"/></g></svg> ANDROID 11+ </li>
+            <li style="text-align: center; font-weight: bold;">Note: You need to bhe connected to wifi</li>
+            <li>Please follow first step(First enable developer options) before cotinuing</li>
+            <li @click="openwifidevsetings">1. <a>On your device, go to Settings > Developer options > wireless debugging.<br>(click this text to go to this setting page)</a></li>
+           <li>2. Click on pair device </li>
+           <li>3. copy  ip address & port (it will be something like 198.1.1.16:5555 - if you are debloating your phone you can just enter port. the number after the colon(":") is port)</li>
+           <li>there will be a notification by app when you open settings by clicking on blue text above , from the notification click "enter port" and paste</li>
+           <li>also copy pairing code and click on "enter code" on app's notification and paste the pairing code</li>
+            <li>NOTE: to return from settings screen back to the app ,press back button</li>
+            <li></li>
+            
+           
+           
           </ul>
         </div>
 
@@ -126,6 +130,7 @@ import { DevOptionsChecker } from '@legendarythedev/dev-options-checker'
 import { ref } from 'vue';
 import { NativeSettings, AndroidSettings, IOSSettings } from "@legendarythedev/capacitor-native-settings"
 import { Toast } from "@capacitor/toast";
+import {LocalNotifications } from '@capacitor/local-notifications';
 let devtoolsenabled: boolean = false;
 DevOptionsChecker.checkDevOptions().then((result: any) => {
   devtoolsenabled = result.enabled;
@@ -152,6 +157,89 @@ function opendevsetings(){
 NativeSettings.open({ optionAndroid: AndroidSettings.ApplicationDevelopment, optionIOS: IOSSettings.App })
 console.log("opening settings > developer options")
 }
+function openwifidevsetings(){
+NativeSettings.open({ optionAndroid: AndroidSettings.ApplicationDevelopment, optionIOS: IOSSettings.App })
+console.log("opening settings > developer options")
+Toast.show({
+    text: "enable wireless debugging and click it\nThen click on pair device",
+    duration: "long",
+  })
+
+  
+  LocalNotifications.registerActionTypes({
+    types: [
+      {
+        id: "1",
+        actions: [
+          {
+            
+            id: "1.1",
+            title: "enter",
+           input: true,
+          
+          }, 
+          {
+            id: "1.2",
+            title: "",
+           
+          },
+        ],
+      },
+    ],
+  });
+LocalNotifications.schedule({
+  notifications: [
+    {
+      title: "enter port and pairing code",
+      body: "enter port and pairing code using buttons below",
+      id: 1,
+     ongoing: true,
+     actionTypeId: "1",
+      
+      
+      extra: null
+    }
+   
+  ]
+});
+LocalNotifications.addListener("localNotificationActionPerformed", (notification) => {
+  console.log("notification:", notification);
+  if (notification.actionId == "1.1") {
+    const ipport = notification.inputValue
+    if (!ipport || ipport == "") {
+      Toast.show({
+        text: "pls input ip address & port",
+        duration: "long",
+      })
+    } else {
+      if (ipport.includes(":")) {
+        const ip = ipport.split(":")[0]
+        const port = ipport.split(":")[1]
+        console.log("ip: " + ip + " port: " + port);
+        Toast.show({
+          text: "ip: " + ip + " port: " + port,
+          duration: "long",
+        })
+      } else {
+        Toast.show({
+          text: "pls input ip address & port",
+          duration: "long",
+        })
+      }
+    }
+
+    console.log("port entered " + notification.inputValue);
+  
+  } else if (notification.actionId == "1.2") {
+    console.log("enter code");
+    Toast.show({
+    text: "enter code",
+    duration: "long",
+  })
+  }
+});
+}
+
 let showDropdown = ref(false);
 function toggleDropdown() {
   console.log('showDropdown:', showDropdown);
